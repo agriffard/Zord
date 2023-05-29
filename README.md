21241e0dd1363fa7fa1b6a45debb078797609a85
Antoine Griffard <agriffard@hotmail.com>
2023-05-29 11:29:16 +0200

    1.6.0

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Zord
+
+## Recipe
+
+## Features
+
+- [X] Admin dashboard.
+- [X] Manage Content types (Page).
+- [X] Edit contents.
+- [X] Setup Recipe.
+- [X] Razor page using IOrchardHelper.
+- [X] Get current site from SiteService.
+- [X] User: Current.
+- [X] Queries.
+- [X] LiquidTemplate component.
+- [] Navigation (Menu shape or query?, hierarchical?).
+- [] List, Bag?
+- [] Media (assets, field).
+- [] Search?
+- [] Table of contents?
+- [] CRUD?
+- [] Custom Settings (from SiteService)?
+- [] Localization?
--- Zord.App/Program.cs
+++ Zord.App/Program.cs
@@ -1,10 +1,7 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Zord.App.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddOrchardCms()
     .ConfigureServices(svc => {
         svc.AddServerSideBlazor();
@@ -13,28 +10,11 @@ builder.Services.AddOrchardCms()
     .Configure((a, b, c) => {
         b.MapBlazorHub();
         a.UseStaticFiles();
-        b.MapFallbackToAreaPage("/_Host", "Zord.App");
+        //b.MapFallbackToAreaPage("/_Host", "Zord.App");
     });
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
 app.UseOrchardCore();
-
-//app.MapBlazorHub();
-//app.MapFallbackToPage("/_Host");
 
 app.Run();
